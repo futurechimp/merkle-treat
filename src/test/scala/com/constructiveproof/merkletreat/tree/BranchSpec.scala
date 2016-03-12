412f0fa6c33cc9d8f22ccfae5d7bed7efed58347
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2016-03-12 09:35:15 +0000

    Clarifying meanings a bit

--- src/test/scala/com/constructiveproof/merkletreat/tree/BranchSpec.scala
+++ src/test/scala/com/constructiveproof/merkletreat/tree/BranchSpec.scala
@@ -1,10 +1,20 @@
 package com.constructiveproof.merkletreat.tree
 
+import com.constructiveproof.merkletreat.stores.MapStore
 import com.constructiveproof.merkletreat.testsupport.TestStack
+import com.roundeights.hasher.Implicits._
 
 class BranchSpec extends TestStack {
 
   describe("A branch in the Merkle tree") {
+    val store = new MapStore
+    val branch = Branch(store, "pivot", "leftId", "rightId")
+
+    describe("identity") {
+      it("should be a SHA256 hash of the branch properties prepended by the letter B") {
+        branch.identity shouldEqual ("B" + "pivot" + "leftId" + "rightId").sha256.hex.toString
+      }
+    }
 
   }
 
