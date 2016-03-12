642ecd76dac1cbe04ac7a1e65551adbd63f01a88
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2016-03-12 08:37:18 +0000

    Adding a spec for Branches

---
+++ src/test/scala/com/constructiveproof/merkletreat/BranchSpec.scala
@@ -0,0 +1,11 @@
+package com.constructiveproof.merkletreat
+
+import com.constructiveproof.merkletreat.testsupport.TestStack
+
+class BranchSpec extends TestStack {
+
+  describe("A branch in the Merkle tree") {
+
+  }
+
+}
