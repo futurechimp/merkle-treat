ef9296735fb6828c5471a14387a638b7baadbea4
Dave Hrycyszyn <dave@constructiveproof.com>
2016-03-15 19:56:52 +0000

    ...which means this test has just been implemented.

--- src/test/scala/com/constructiveproof/merkletreat/tree/LeafSpec.scala
+++ src/test/scala/com/constructiveproof/merkletreat/tree/LeafSpec.scala
@@ -54,13 +54,6 @@ class LeafSpec extends TestStack {
           store.retrieve(newBranch.identity) shouldEqual newBranch
         }
       }
-
-      ignore("when the item has previously been added to the tree") {
-        // In a console, adding twice results in the exact same mapstore
-        // contents, and it doesn't seem worth it to add the guard as it'll
-        // mean returning multiple types === superclass. Ask George about it.
-        it("should not add anything at all") {}
-      }
     }
   }
 
