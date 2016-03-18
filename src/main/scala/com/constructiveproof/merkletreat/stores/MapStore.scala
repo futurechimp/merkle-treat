7a5f9d4e495623770f32dbe7fa3ea4b4854a6d86
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2016-03-18 20:25:30 +0000

    Docs on mapStore

--- src/main/scala/com/constructiveproof/merkletreat/stores/MapStore.scala
+++ src/main/scala/com/constructiveproof/merkletreat/stores/MapStore.scala
@@ -3,6 +3,9 @@ package com.constructiveproof.merkletreat.stores
 import com.constructiveproof.merkletreat.tree.{Branch, Leaf, Node}
 import scala.collection.mutable
 
+/**
+  * A default RAM-based store, implemented as a Scala mutable.Map.
+  */
 class MapStore extends DataStore {
 
   private val map = mutable.Map[String, Node]()
